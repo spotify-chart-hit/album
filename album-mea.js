e923d7b5ae6f8c3efcc1f398887bab8ed946000b
wonytest0-dev <wonytest0@gmail.com>
2026-05-15 08:07:03 +0700

    Update album-mea.js

--- album-mea.js
+++ album-mea.js
@@ -1,1 +1,409 @@
+const fs = require("fs");
+const getToken = require("./auto-token");
+
+const sleep = ms =>
+new Promise(resolve =>
+setTimeout(resolve, ms)
+);
+
+const countries = [
+
+"EG", "IL", "MA", "NG", "SA", "ZA", "TR", "AE"
+
+];
+
+async function getLatestDates(
+token
+) {
+
+let weekly;
+
+while (true) {
+
+weekly =
+await fetch(
+
+"https://charts-spotify-com-service.spotify.com/auth/v0/charts/album-global-weekly/latest",
+
+{
+headers: {
+Authorization: token
+}
+}
+
+);
+
+if (
+
+weekly.status === 429
+
+) {
+
+console.log(
+"429 latestDate 😭"
+);
+
+await sleep(
+8000
+);
+
+continue;
+
+}
+
+break;
+
+}
+
+const weeklyJson =
+await weekly.json();
+
+return {
+
+weekly:
+weeklyJson.latestDate
+
+};
+
+}
+
+async function scrape(
+token
+) {
+
+console.log(
+"SCRAPING ALBUM MEA 😭🔥"
+);
+
+let results = [];
+
+for (
+
+const country
+of countries
+
+) {
+
+for (
+
+const type
+of ["weekly"]
+
+) {
+
+try {
+
+const url =
+
+`https://charts-spotify-com-service.spotify.com/auth/v0/charts/album-${country.toLowerCase()}-${type}/latest`;
+
+console.log(
+`CHECKING ${country} ${type}`
+);
+
+let response =
+await fetch(
+
+url,
+
+{
+headers: {
+Authorization: token,
+Accept:
+"application/json"
+}
+}
+
+);
+
+while (
+
+response.status ===
+429
+
+) {
+
+console.log(
+`429 😭 ${country} ${type}`
+);
+
+await sleep(
+8000
+);
+
+response =
+await fetch(
+
+url,
+
+{
+headers: {
+Authorization: token,
+Accept:
+"application/json"
+}
+}
+
+);
+
+}
+
+if (
+
+response.status !==
+200
+
+) {
+
+continue;
+
+}
+
+const data =
+await response.json();
+
+const tracks =
+data.entries
+||
+data.chartEntryViewResponses
+||
+[];
+
+for (
+
+const track
+of tracks
+
+) {
+
+const artists =
+
+track.albumMetadata
+?.artists
+||
+[];
+
+const hasJimin =
+
+artists.some(
+
+artist =>
+
+artist.name
+?.toLowerCase()
+
+=== "jimin"
+
+);
+
+if (
+
+hasJimin
+
+) {
+
+results.push({
+
+country,
+type,
+
+rank:
+
+track.chartEntryData
+?.currentRank,
+
+previousRank:
+
+track.chartEntryData
+?.previousRank,
+
+peakRank:
+
+track.chartEntryData
+?.peakRank,
+
+appearances:
+
+track.chartEntryData
+?.appearancesOnChart,
+
+album:
+
+track.albumMetadata
+?.albumName,
+
+artists:
+
+artists.map(
+a => a.name
+),
+
+image:
+
+track.albumMetadata
+?.displayImageUri
+
+});
+
+console.log(
+
+`FOUND 😭🔥 ${country} ${track.albumMetadata?.albumName}`
+
+);
+
+}
+
+}
+
+await sleep(
+800
+);
+
+}
+
+catch (err) {
+
+console.log(
+err.message
+);
+
+}
+
+}
+
+}
+
+fs.writeFileSync(
+
+"album-mea.json",
+
+JSON.stringify(
+results,
+null,
+2
+)
+
+);
+
+console.log(
+"UPDATED album-mea.json 😍"
+);
+
+}
+
+async function start() {
+
+const token =
+await getToken();
+
+let savedDates =
+null;
+
+if (
+
+fs.existsSync(
+"chart-album-mea.json"
+)
+
+) {
+
+savedDates =
+JSON.parse(
+
+fs.readFileSync(
+"chart-album-mea.json"
+)
+
+);
+
+}
+
+const latest =
+await getLatestDates(
+token
+);
+
+const firstRun =
+
+!savedDates
+||
+
+!fs.existsSync(
+"album-mea.json"
+);
+
+if (
+
+firstRun
+
+) {
+
+console.log(
+"FIRST RUN 😍"
+);
+
+await scrape(
+token
+);
+
+fs.writeFileSync(
+
+"chart-album-mea.json",
+
+JSON.stringify(
+latest,
+null,
+2
+)
+
+);
+
+return;
+
+}
+
+const changed =
+
+latest.weekly !==
+savedDates.weekly;
+
+if (
+
+changed
+
+) {
+
+console.log(
+"NEW CHART 😍"
+);
+
+await scrape(
+token
+);
+
+fs.writeFileSync(
+
+"chart-album-mea.json",
+
+JSON.stringify(
+latest,
+null,
+2
+)
+
+);
+
+}
+
+else {
+
+console.log(
+"SAME CHART 😴"
+);
+
+}
+
+}
+
+start();
 
