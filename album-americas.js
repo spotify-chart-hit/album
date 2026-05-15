628d206b8fde7000c340bcda832f5411d937f61a
wonytest0-dev <wonytest0@gmail.com>
2026-05-15 09:42:11 +0700

    Update album-americas.js

--- album-americas.js
+++ album-americas.js
@@ -66,9 +66,21 @@ await weekly.json();
 return {
 
 weekly:
-weeklyJson.latestDate
+
+weeklyJson
+?.displayChart
+?.chartMetadata
+?.dimensions
+?.latestDate
+
+||
+
+weeklyJson
+?.displayChart
+?.date
 
 };
+
 
 }
 
