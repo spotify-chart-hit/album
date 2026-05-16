9e2903e19ba689fb912a4a7455ecebad02bc0734
wonytest0-dev <wonytest0@gmail.com>
2026-05-17 01:15:49 +0700

    Update album-mea.js

--- album-mea.js
+++ album-mea.js
@@ -76,8 +76,6 @@ weeklyJson
 
 };
 
-
-
 }
 
 async function scrape(
@@ -215,20 +213,93 @@ hasJimin
 
 ) {
 
+const currentRank =
+
+track.chartEntryData
+?.currentRank;
+
+const previousRank =
+
+track.chartEntryData
+?.previousRank;
+
+const rankChange =
+
+previousRank
+? Math.abs(
+currentRank -
+previousRank
+)
+: 0;
+
+let direction = "=";
+let entryStatus = null;
+
+// NEW ENTRY
+if (
+
+previousRank === null
+||
+previousRank === undefined
+
+) {
+
+entryStatus =
+"NEW_ENTRY";
+
+}
+
+// RE-ENTRY
+else if (
+
+rankChange >= 100
+
+) {
+
+entryStatus =
+"RE_ENTRY";
+
+}
+
+// NORMAL MOVEMENT
+else {
+
+if (
+
+currentRank <
+previousRank
+
+) {
+
+direction =
+"up";
+
+}
+
+else if (
+
+currentRank >
+previousRank
+
+) {
+
+direction =
+"down";
+
+}
+
+}
+
 results.push({
 
 country,
 type,
 
 rank:
-
-track.chartEntryData
-?.currentRank,
+currentRank,
 
 previousRank:
-
-track.chartEntryData
-?.previousRank,
+previousRank,
 
 peakRank:
 
@@ -254,7 +325,11 @@ a => a.name
 image:
 
 track.albumMetadata
-?.displayImageUri
+?.displayImageUri,
+
+rankChange,
+direction,
+entryStatus
 
 });
 
@@ -419,4 +494,3 @@ console.log(
 }
 
 start();
-
